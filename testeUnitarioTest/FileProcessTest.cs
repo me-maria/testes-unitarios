b23a8ba53fa22806797891219b280c476fee7458
Maria Eduarda Corrêa <133533208+me-maria@users.noreply.github.com>
2023-09-26 15:56:07 -0300

    add teste de verificação de existencia de arquivo

---
+++ testeUnitarioTest/FileProcessTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using testeUnitario;
+
+namespace testeUnitarioTest
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+     
+            [TestMethod] // Teste de verificação de existência de arquivo 
+            public void FileNameDoesExist()
+            {
+                FileProcess fp = new FileProcess(); 
+
+                bool fromcall = fp.FileExists(@"C:\Users\maria\OneDrive\Documentos\atividades\api.txt"); 
+
+                Assert.IsTrue(fromcall); 
+            }
+
+
+    }
+}
---
+++ testeUnitarioTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("testeUnitarioTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("testeUnitarioTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2f3410be-8936-4209-a570-928b37de621c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
