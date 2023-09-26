470f39c5b453063c34cbed79d91035bbf333b5d4
Maria Eduarda Corrêa <133533208+me-maria@users.noreply.github.com>
2023-09-26 12:06:10 -0300

    criando método a ser testado

---
+++ testeUnitario/FileProcess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+namespace testeUnitario
+{
+    public class FileProcess
+    {
+        public bool FileExists(string fileName)
+        {
+            if(string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException("fileName");
+            }
+            return FileExists(fileName);
+        }
+    }
+}
